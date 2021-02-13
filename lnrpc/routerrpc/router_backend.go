677b41b452c6ec72e6a7268135b949041d4a6ab7
Elliott Jin <elliott.jin@gmail.com>
2021-02-12 23:29:13 -0800

    routerrpc: expose ChanStateManager methods

--- lnrpc/routerrpc/router_backend.go
+++ lnrpc/routerrpc/router_backend.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
-
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/htlcswitch"
@@ -74,6 +74,11 @@ type RouterBackend struct {
 	// InterceptableForwarder exposes the ability to intercept forward events
 	// by letting the router register a ForwardInterceptor.
 	InterceptableForwarder htlcswitch.InterceptableHtlcForwarder
+
+	// SetChannel* exposes the ability to manually set the status of a channel.
+	SetChannelEnabled  func(wire.OutPoint) error
+	SetChannelDisabled func(wire.OutPoint) error
+	SetChannelAuto     func(wire.OutPoint) error
 }
 
 // MissionControl defines the mission control dependencies of routerrpc.
